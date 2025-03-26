5bbe2d6cff470da26b003f57b002432b5f71c42b
Marta <159387209+martaic@users.noreply.github.com>
2025-03-26 11:54:09 +0000

    Pending changes exported from your codespace

---
+++ app.js
@@ -0,0 +1,24 @@
+
+let oneEuroIs = {
+    "JPY": 156.5,
+    "USD": 1.07,
+    "GBP": 0.87
+};
+
+const fromEuroToDollar = function(valueInEuro) {
+    return valueInEuro * oneEuroIs.USD;
+};
+
+const fromDollarToYen = function(valueInDollar) {
+    let valueInEuro = valueInDollar / oneEuroIs.USD;
+    return valueInEuro * oneEuroIs.JPY;
+};
+
+const fromYenToPound = function(valueInYen) {
+    let valueInEuro = valueInYen / oneEuroIs.JPY;
+    return valueInEuro * oneEuroIs.GBP;
+};
+
+
+
+module.exports = { fromEuroToDollar, fromDollarToYen, fromYenToPound};
